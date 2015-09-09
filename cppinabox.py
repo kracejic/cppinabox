5c380a62e71f6c34e2095fb707ac4e537ff9c670
kracejic <kracejic@gmail.com>
2015-09-09 20:37:43 +0200

    wip

--- cppinabox.py
+++ cppinabox.py
@@ -4,5 +4,6 @@ from .lib.settings import Settings
 class CppinaboxdebugtestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         print("test = " + str(Settings.get(self.view, "test", "default")))
+        print("Active window - " + sublime.active_window().project_file_name())
 
 
---
+++ ycm/__init__.py
@@ -0,0 +1,11 @@
+
+# from .autocomplete import CppYCMCompletionsListener
+# from .highlight_problems import CppYCMHighlightProblemsListener
+# from .loadextraconf import CppYCMLoadExtraConfListener
+
+
+# __all__ = [
+#     'CppYCMCompletionsListener',
+#     'CppYCMHighlightProblemsListener',
+#     'CppYCMLoadExtraConfListener'
+# ]
